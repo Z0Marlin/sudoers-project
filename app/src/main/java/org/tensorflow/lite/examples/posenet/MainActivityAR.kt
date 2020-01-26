75617613fa135efc18cafd5a8b8df66ba518abee
Arpitha <arpitharaghunandan@gmail.com>
2020-01-26 15:23:50 +0530

    AR

--- app/src/main/java/org/tensorflow/lite/examples/posenet/MainActivityAR.kt
+++ app/src/main/java/org/tensorflow/lite/examples/posenet/MainActivityAR.kt
@@ -1,9 +1,18 @@
 package org.tensorflow.lite.examples.posenet
 
+import android.graphics.Point
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
+import com.google.ar.core.Anchor
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main_ar.*
 
 class MainActivityAR : AppCompatActivity() {
@@ -18,6 +27,54 @@ class MainActivityAR : AppCompatActivity() {
 
         fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
 
+        fab.setOnClickListener {
+            addObject(Uri.parse("Heart.sfb"))
+        }
     }
 
+    private fun addObject(parse: Uri) {
+        val frame = fragment.arSceneView.arFrame
+        val point = getScreenCenter()
+        if (frame != null) {
+            val hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    placeObject(fragment, hit.createAnchor(), parse)
+                    break
+                }
+            }
+        }
+    }
+
+    private fun placeObject(fragment: ArFragment, createAnchor: Anchor, model: Uri) {
+        ModelRenderable.builder()
+            .setSource(fragment.context, model)
+            .build()
+            .thenAccept {
+                addNodeToScene(fragment, createAnchor, it)
+            }
+            .exceptionally {
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage(it.message)
+                    .setTitle("error!")
+                val dialog = builder.create()
+                dialog.show()
+                return@exceptionally null
+            }
+    }
+
+    private fun addNodeToScene(fragment: ArFragment, createAnchor: Anchor, renderable: ModelRenderable) {
+        val anchorNode = AnchorNode(createAnchor)
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderable
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+
+    private fun getScreenCenter(): android.graphics.Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return Point(vw.width / 2, vw.height / 2)
+    }
 }
